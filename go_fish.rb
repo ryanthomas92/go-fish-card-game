2692c3d743be7910f0b26682874c1f23febb19f8
ryanthomas92 <rthoma11@villanova.edu>
2017-01-20 15:21:30 -0800

    finished classwork

--- go_fish.rb
+++ go_fish.rb
@@ -1,9 +1,34 @@
 class PlayingCard
+  RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  SUITS = ["C", "D", "H", "S"]
+  SORTED_CARDS = [
+    "AC", "AD", "AH", "AS", "2C", "2D", "2H", "2S", "3C", "3D", "3H", "3S",
+    "4C", "4D", "4H", "4S", "5C", "5D", "5H", "5S", "6C", "6D", "6H", "6S",
+    "7C", "7D", "7H", "7S", "8C", "8D", "8H", "8S", "9C", "9D", "9H", "9S",
+    "10C", "10D", "10H", "10S", "JC", "JD", "JH", "JS", "QC", "QD", "QH", "QS",
+    "KC", "KD", "KH", "KS"
+  ]
+  attr_reader :rank, :suit, :face
+
   # initialize
+  def initialize(arg)
+    @rank = arg[:rank]
+    @suit = arg[:suit]
+    @face = @rank + @suit
+  end
+
+  def face_to_s
+    @face.to_s
+  end
+
 end
 
 class CardDeck
+  attr_reader
   # initialize
+  def initialize
+    
+  end
 end
 
 class HandOfCards
@@ -18,6 +43,34 @@ end
 # Driver Code
 if __FILE__ == $0
   puts "This will only print if you run `ruby go_fish.rb`"
+  deck = CardDeck.new
+  deck.shuffle
+
+  card = deck.draw_one
+  two_cards = deck.draw(2)
+
+  cards1 = deck.draw(5)
+  h1 = HandOfCards.new(cards1)
+  p1 = CardPlayer.new(hand: h1)
+
+  cards2 = deck.draw(5)
+  h2 = HandOfCards.new(cards2)
+  p2 = CardPlayer.new(hand: h2 )
+
+
+  # GAME PLAY
+
+  wanted_rank = "3"
+  puts "p1, do you have any... #{wanted_rank}'s?"
+  if p1.hand.any?(rank: wanted_rank)
+    cards = p1.hand.take!(rank: wanted_rank)
+    p2.hand.push(*cards)
+  else
+    puts "Go Fish"
+    drawn_card = deck.draw
+    p2.hand.push(drawn_card)
+  end
+
   # deck = CardDeck.new
   # # # puts "cards: #{deck.cards}"
   # # # puts "cards: #{deck}"
